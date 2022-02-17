6b32e7c1da98f1c613b3766498ac2f132fcdf975
Renan Cruz <rcruz.vermont@alelo.com.br>
2022-02-17 00:39:27 -0300

    arrumando formulários

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,50 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
   def index
     @articles = Article.all
   end
 
   def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article  = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else 
+      render :new
+    end
+  end
+  
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    if @article.destroy
+      redirect_to root_path
+    else 
+      render :show
+    end
+  end
+
+  private
+
+  def set_article
     @article = Article.find(params[:id])
+  end
+  def article_params
+    params.require(:article).permit(:title,:body)
   end
 end
