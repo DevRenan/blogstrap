586c4108183615b2164b5bad406a0521160a2d27
Renan Cruz <rcruz.vermont@alelo.com.br>
2022-03-16 21:12:16 -0300

    internacionalização

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
+  around_action :switch_locale
   private
+
+  def switch_locale
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :categories, except: [:show]
-  root 'articles#index'
-  
-  resources :articles do
-    resources :comments, only: %i[create destroy]
-  end
+  scope'(:locale)', locale: /pt-BR|en/ do
+    devise_for :users
+    resources :categories, except: [:show]
+    root 'articles#index'
+    
+    resources :articles do
+      resources :comments, only: %i[create destroy]
+    end
 
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  end
 end
